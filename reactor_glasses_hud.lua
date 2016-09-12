5244ddd54183b829f39e6d75d6ddc85f403ce6c5
Jonas <thenixchecker@gmail.com>
2016-09-12 20:49:15 +0200

    Update reactor_glasses_hud.lua

--- reactor_glasses_hud.lua
+++ reactor_glasses_hud.lua
@@ -1,32 +1,42 @@
 comp = require "component"
-event = require "event"
-serialization = require "serialization"
-g = comp.glasses
-m = comp.modem
+    event = require "event"
+    serialization = require "serialization"
+    g = comp.glasses
+    m = comp.modem
 
-g.removeAll()
+    g.removeAll()
 
-function addInfoText(text)
-    local label = g.addTextLabel()
-    label.setPosition(10, 50)
-    label.setScale(1)
-    label.setColor(255, 20 , 20)
-    label.setAlpha(0.8)
-    label.setText(text)
-end
+    base_y = 50;
+    x = 10;
 
-addInfoText("Waiting for signal")
-
-r_data = {};
-
-m.open(8000)
-
-while true do
-    local _, receiver, sender, port, _, message = event.pull("modem_message")
-    --print("Got a message from " .. sender .. " on port " .. receiver .. ":"..port..": " .. tostring(message))
-    r_data = serialization.unserialize(message)
-    for k, v in pairs(r_data) do
-        addInfoText(k..": "..v)
+    function addInfoText(text, iteration)
+        local label = g.addTextLabel()
+        label.setPosition(x, 50 + iteration*10)
+        label.setScale(1)
+        label.setColor(255, 20 , 20)
+        label.setAlpha(0.8)
+        label.setText(text)
     end
-    os.sleep(1)
-end
+
+    addInfoText("Waiting for signal", 0)
+
+
+    m.open(8000)
+
+    while true do
+        local counter = 0
+        local _, receiver, sender, port, _, message = event.pull("modem_message")
+        local r_data = serialization.unserialize(message)
+        g.removeAll()
+        for k, v in pairs( r_data ) do
+            if k == "IsActive" and v == false then
+                g.removeAll()
+                addInfoText("Reactor offline", 0)
+                break
+            elseif k ~= "IsActive" then
+                addInfoText(k..": "..tostring(v), counter)
+                counter = counter + 1
+            end
+        end
+        os.sleep(1)
+    end
