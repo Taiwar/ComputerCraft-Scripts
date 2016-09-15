9bfb80faa8318e8c0e341983a3c04d70eddd324b
Jonas <thenixchecker@gmail.com>
2016-09-15 19:33:13 +0200

    Update reactor_monitor.lua

--- reactor_monitor.lua
+++ reactor_monitor.lua
@@ -6,10 +6,11 @@ r = comp.br_reactor
 r_data = {}
 
 while true do
-    r_data["IsActive"] = r.getActive()
-    r_data["Fuel in ml"] = r.getFuelAmount()
-    r_data["Waste in ml"] = r.getWasteAmount()
-    r_data["EnergyOutput in RF/t"] = math.floor(r.getEnergyProducedLastTick())
+    r_data[1] = r.getActive()
+    r_data[2] = r.getFuelAmount()
+    r_data[3] = r.getWasteAmount()
+    r_data[4] = math.floor(r.getEnergyProducedLastTick())
+    r_data[5] = r.getFuelAmountMax()
     m.send("e853c07a-6923-49e9-97fe-63c9dd1ed497", 8000, serialization.serialize(r_data))
     os.sleep(1)
 end
