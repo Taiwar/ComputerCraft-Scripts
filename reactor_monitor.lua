00ff8b86e0cf24ca54bacea63e7e391fc5006381
Jonas <thenixchecker@gmail.com>
2016-09-17 00:33:24 +0200

    Update reactor_monitor.lua

--- reactor_monitor.lua
+++ reactor_monitor.lua
@@ -3,16 +3,19 @@ event = require "event"
 serialization = require "serialization"
 m = comp.modem
 r = comp.br_reactor
-r_data = {}
+c = comp.capacitor_bank
+msg_data = {}
 
 hub_adress = "e853c07a-6923-49e9-97fe-63c9dd1ed497"
 
 while true do
-    r_data[1] = r.getActive()
-    r_data[2] = r.getFuelAmount()
-    r_data[3] = r.getWasteAmount()
-    r_data[4] = math.floor(r.getEnergyProducedLastTick())
-    r_data[5] = r.getFuelAmountMax()
-    m.send(hub_adress, 8000, serialization.serialize(r_data))
+    msg_data[1] = r.getActive()
+    msg_data[2] = r.getFuelAmount()
+    msg_data[3] = r.getWasteAmount()
+    msg_data[4] = math.floor(r.getEnergyProducedLastTick())
+    msg_data[5] = r.getFuelAmountMax()
+    msg_data[6] = c.getEnergyStored()
+    msg_data[7] = c.getMaxEnergyStored()
+    m.send(hub_adress, 8000, serialization.serialize(msg_data))
     os.sleep(1)
 end
