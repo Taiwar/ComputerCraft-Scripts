d494b62f7c41a8a10010471478c5f51b8fba091d
Jonas <thenixchecker@gmail.com>
2016-09-16 17:32:12 +0200

    Update reactor_monitor.lua

--- reactor_monitor.lua
+++ reactor_monitor.lua
@@ -5,12 +5,14 @@ m = comp.modem
 r = comp.br_reactor
 r_data = {}
 
+hub_adress = "e853c07a-6923-49e9-97fe-63c9dd1ed497"
+
 while true do
     r_data[1] = r.getActive()
     r_data[2] = r.getFuelAmount()
     r_data[3] = r.getWasteAmount()
     r_data[4] = math.floor(r.getEnergyProducedLastTick())
     r_data[5] = r.getFuelAmountMax()
-    m.send("e853c07a-6923-49e9-97fe-63c9dd1ed497", 8000, serialization.serialize(r_data))
+    m.send(hub_adress, 8000, serialization.serialize(r_data))
     os.sleep(1)
 end
