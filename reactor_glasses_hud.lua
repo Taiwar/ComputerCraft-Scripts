1020cd4208a0d144f96ddcb6239ad58ad09e58d4
Jonas <thenixchecker@gmail.com>
2016-09-17 00:33:00 +0200

    Update reactor_glasses_hud.lua

--- reactor_glasses_hud.lua
+++ reactor_glasses_hud.lua
@@ -8,11 +8,17 @@ g.removeAll()
 
 base_y = 40
 base_x = 10
+base_width = 100
 base_text_scale = 0.8
 primary_color = {1, 1, 1 }
+--primary_color = {0.467, 0, 1 }
+primary_color_dark = {primary_color[1] - 0.2, primary_color[2] - 0.2, primary_color[3] - 0.2}
 
 waste_box = 0
 fuel_box = 0
+r_capacity_end_box = 0
+r_capacity_start_box = 0
+energy_box = 0
 capacity_end_box = 0
 capacity_start_box = 0
 
@@ -38,15 +44,19 @@ function addInfoText(text, y)
     return label
 end
 
-function initFuelDisplay()
+function initFuelDisplay(y)
     waste_box = g.addRect()
     fuel_box = g.addRect()
     capacity_end_box = g.addRect()
     capacity_start_box = g.addRect()
 
-    capacity_start_box.setColor(primary_color[1], primary_color[2] , primary_color[3])
+    capacity_start_box.setSize(10.8, 0.8)
+    capacity_start_box.setPosition(base_x - 0.8, y-0.4)
+    capacity_start_box.setColor(primary_color_dark[1], primary_color_dark[2] , primary_color_dark[3])
 
-    capacity_end_box.setColor(primary_color[1], primary_color[2] , primary_color[3])
+    capacity_end_box.setSize(10.8, 0.8)
+    capacity_end_box.setPosition(base_x + base_width, y-0.4)
+    capacity_end_box.setColor(primary_color_dark[1], primary_color_dark[2] , primary_color_dark[3])
 
     waste_box.setColor(0.1059, 0, 0.902)
     waste_box.setAlpha(0.9)
@@ -55,40 +65,57 @@ function initFuelDisplay()
     fuel_box.setAlpha(0.9)
 end
 
+function initPowerDisplay(y)
+    energy_box = g.addRect()
+    capacity_end_box = g.addRect()
+    capacity_start_box = g.addRect()
+
+    capacity_start_box.setSize(10.8, 0.8)
+    capacity_start_box.setPosition(base_x - 0.8, y-0.4)
+    capacity_start_box.setColor(primary_color_dark[1], primary_color_dark[2] , primary_color_dark[3])
+
+    capacity_end_box.setSize(10.8, 0.8)
+    capacity_end_box.setPosition(base_x + base_width, y-0.4)
+    capacity_end_box.setColor(primary_color_dark[1], primary_color_dark[2] , primary_color_dark[3])
+
+    --energy_box.setColor(0.3019, 0.8196, 0.8745)
+    energy_box.setColor(primary_color[1], primary_color[2] , primary_color[3])
+    energy_box.setAlpha(0.9)
+end
+
 function updateFuelDisplay(fuel, waste, capacity, y)
-    local base_width = 100
     local waste_ratio = waste/capacity
     local fuel_ratio = fuel/capacity
     local waste_width = waste_ratio*100
     local fuel_width = fuel_ratio*100
-    local capacity_width = base_width - fuel_width + waste_width
-    local waste_percentage = tostring(waste_ratio*100).."%"
-
-    capacity_start_box.setSize(10.8, 0.8)
-    capacity_start_box.setPosition(base_x - 0.8, y-0.4)
-
-    capacity_end_box.setSize(10.8, 0.8)
-    capacity_end_box.setPosition(base_x + fuel_width + waste_width + capacity_width, y-0.4)
+    --local waste_percentage = tostring(waste_ratio*100).."%"
 
     waste_box.setSize(10, waste_width)
     waste_box.setPosition(base_x + fuel_width, y)
 
     fuel_box.setSize(10, fuel_width)
     fuel_box.setPosition(base_x, y)
-
 end
 
-function addBgBox()
+function updatePowerDisplay(energy, capacity, y)
+    local energy_ratio = energy/capacity
+    local energy_width = energy_ratio*100
+
+    energy_box.setSize(10, energy_width)
+    energy_box.setPosition(base_x, y)
+end
+
+function addBgBox(y)
     local bg_box_prim = g.addRect()
     local bg_box_sec = g.addRect()
 
     bg_box_prim.setSize(36, 134)
-    bg_box_prim.setPosition(base_x - 6, base_y - 12)
-    bg_box_prim.setColor(primary_color[1], primary_color[2] , primary_color[3])
+    bg_box_prim.setPosition(base_x - 6, y - 12)
+    bg_box_prim.setColor(primary_color_dark[1], primary_color_dark[2] , primary_color_dark[3])
     bg_box_prim.setAlpha(0.4)
 
     bg_box_sec.setSize(32, 130)
-    bg_box_sec.setPosition(base_x - 4, base_y - 10)
+    bg_box_sec.setPosition(base_x - 4, y - 10)
     bg_box_sec.setColor(0, 0 , 0)
     bg_box_sec.setAlpha(0.6)
 end
@@ -96,12 +123,15 @@ end
 m.open(8000)
 m.open(8001)
 
-addBgBox()
-energy_info = addInfoText("", base_y)
-waste_info = addInfoText("", base_y + 10)
-initFuelDisplay()
+addBgBox(base_y)
+addBgBox(base_y + 38)
+production_info = addInfoText("", base_y)
+waste_info = addInfoText("Reactor fuel status:", base_y + 10)
+power_info = addInfoText("Capacitor bank power:", base_y + 48)
+initFuelDisplay(base_y + 10)
+initPowerDisplay(base_y + 45)
 
-energy_info.setText("Waiting for signal")
+production_info.setText("Waiting for signal")
 
 while true do
     local _, _, _, port, _, message = event.pull("modem_message")
@@ -109,11 +139,12 @@ while true do
 
     if port == 8000 then
         if msg[1] then
-            energy_info.setText("Energy output in RF/t: "..msg[4])
-            waste_info.setText("Reactor fuel status: ")
+            production_info.setText("Energy output in kRF/t: "..msg[4]/1000)
+            power_info.setText("Energy stored in kRF: "..msg[6]/1000)
             updateFuelDisplay(msg[2], msg[3], msg[5], base_y + 10)
+            updatePowerDisplay(msg[6], msg[7], base_y + 45)
         else
-            energy_info.setText("Reactor offline")
+            production_info.setText("Reactor offline")
         end
     elseif port == 8001 then
         print("executing function: "..msg[1])
